5ca162a24a5b5a1fc9d0e7b7cdb786fe7b459e7b
DhruvDhruvDhruvDhruv <DhruvDurv@gmail.com>
2025-01-06 11:51:57 +0000

    midway combining both scenes - pre-globalisation

--- boozeshotroulette/scripts/shotgun_scene.gd
+++ boozeshotroulette/scripts/shotgun_scene.gd
@@ -2,12 +2,15 @@ extends Node2D
 @onready var animation_player: AnimationPlayer = $CanvasLayer/Shotgun/AnimationPlayer
 @onready var shotgunsound: AudioStreamPlayer2D = $shotgunsound
 @onready var shotreset_timer: Timer = $shotreset_timer
+@onready var popup_box: Control = $CanvasLayer/PopupBox
+signal shots_send(value : Array)
 
 var shots : Array = [] # 1 is a shot, 0 is a blank
 var shot_reset : bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	await get_tree().create_timer(0.5).timeout
 	reload_gun()
 
 
@@ -33,17 +36,26 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 		shot_reset = false
 		shotreset_timer.start()
 		shots.pop_front()
+		if shots.is_empty():
+			reload_gun()
 		if is_a_shot:
 			fire_shot()
 		else:
 			fire_blank()
-	if shots.is_empty():
-		reload_gun()
 		
 		
 func reload_gun():
 	for i in range(6):
-		shots.append(randi_range(0,1))
+		if i < randi_range(2,4):
+			shots.append(1)
+		else:
+			shots.append(0)
+	await emit_signal("shots_send",shots)
+	await get_tree().create_timer(0.5).timeout
+	if popup_box.visible:
+		get_tree().create_timer(0.5).timeout
+	shots.shuffle()
+	print(shots)
 
 func _on_shotreset_timer_timeout() -> void:
 	shot_reset = true
